c07311f1a58ff38e041d38783afdf5fb515fa232
Robert <majointa@gmail.com>
2021-07-04 12:28:41 +0200

    fix cors

--- functions/api.js
+++ functions/api.js
@@ -20,12 +20,32 @@ app.use("/en", EnglishRoute);
 app.use("/init", initRoute);
 app.use("/", router);
 
+//cors
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.i-saro.netlify.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //connect DB
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.listen(3000);
 
 module.exports.handler = serverless(app);
