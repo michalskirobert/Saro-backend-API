b4a16b3d67be7710860fbe6774d17af2d3768fdf
Robert <majointa@gmail.com>
2021-07-04 13:09:13 +0200

    fix

--- functions/api.js
+++ functions/api.js
@@ -20,28 +20,33 @@ app.use("/init", initRoute);
 app.use("/", router);
 
 //cors
-const allowedOrigins = ["http://localhost:3000", "https://i-saro.netlify.app"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// const allowedOrigins = ["http://localhost:3000", "https://i-saro.netlify.app"];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin." +
+//           " Api made only for SARO INC";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+
+app.use(cors());
 
 //connect DB
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.listen(3000, () => console.log("CORS 30000"));
 
 module.exports.handler = serverless(app);
