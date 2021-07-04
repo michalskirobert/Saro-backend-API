57cf13f2a9f77fa026782d90185d99e9f867a1cd
Robert <majointa@gmail.com>
2021-07-04 13:26:38 +0200

    fix cors

--- functions/routes/en/index.js
+++ functions/routes/en/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const dictionariesRoute = require("./dictionaries");
 const EnglishRoute = express();
+EnglishRoute.use(cors());
 
 EnglishRoute.use("/dictionaries", dictionariesRoute);
 
