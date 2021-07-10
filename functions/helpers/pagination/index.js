7b0a466f0f124d2ab06586a750d01b946cf019b3
Robert <majointa@gmail.com>
2021-07-10 12:17:47 +0200

    fix params

--- functions/helpers/pagination/index.js
+++ functions/helpers/pagination/index.js
@@ -3,22 +3,10 @@ function paginatedResults(model) {
     const page = parseInt(req.query.page);
     const size = parseInt(req.query.size);
     const startIndex = (page - 1) * size;
-    const endIndex = page * size;
 
     let items = {};
 
     items.totalCount = model.length;
-
-    if (endIndex < (await model.countDocuments().exec())) {
-      items.next = { page: page + 1, size: size };
-    }
-
-    if (startIndex > 0) {
-      items.previous = {
-        page: page - 1,
-        size: size,
-      };
-    }
 
     try {
       items.items = await model.find().limit(size).skip(startIndex).exec();
